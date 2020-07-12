f5a24e5c55af601e4933c5784c433416fdd12e27
Yuvraj Garg <yuvraj97.ml@gmail.com>
2020-07-12 14:22:03 +0530

    Update
    Added "fig()" function in "Transform" class, to get figures of "Original vectors" and "Transformed vectors"

--- Transform.py
+++ Transform.py
@@ -13,20 +13,27 @@ from Plot2D import Plot2DVectors as vt
 class Transform:
     def __init__(self, transform):
         self.transform = transform
-        self._plot_orig_ = vt()
-        self._plot_tf_   = vt()
+        self._plot_orig_ = vt("Original vectors")
+        self._plot_tf_   = vt("Transformed vectors")
         self._fig_       = plt.figure()
-        plt.close()
+        #plt.close()
     
     
-    def transform_vectors(self, vectors, origin=np.array([0,0])):
+    def add_vectors(self, vectors, origin=np.array([0,0])):
         transformed_vectors = np.matmul(self.transform, vectors.T).T
         self._plot_orig_.add_vectors(vectors)
         self._plot_tf_.add_vectors(transformed_vectors)
+        #self._plot_orig_._fig_.show()
+        #self._plot_tf_._fig_.show()
+    
+    def show(self):
         self._plot_orig_._fig_.show()
         self._plot_tf_._fig_.show()
+    
+    def fig(self):
+        return (self._plot_orig_._fig_, self._plot_tf_._fig_)
 
-"""
+
 # Example
 transform = np.array([
                     [1,1],
@@ -39,5 +46,5 @@ vectors = np.array([
                     [1,3],
                    ])
 tf = Transform(transform)
-tf.transform_vectors(vectors)
-"""
+tf.add_vectors(vectors)
+tf.show()
